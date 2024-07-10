9a8551b486da83601e87725b1d2bdf01b4ece1e3
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-07-10 01:25:45 -0400

    completed getmapping annotation

--- jpa/jpa/src/main/java/com/example/jpa/controllers/EmployeeController.java
+++ jpa/jpa/src/main/java/com/example/jpa/controllers/EmployeeController.java
@@ -17,13 +17,14 @@ public class EmployeeController {
 //    }
 
     @GetMapping(path = "/{employeeID}")
-    public EmployeeDTO getEmployeeById(@PathVariable Long employeeID){
-        return new EmployeeDTO(employeeID, "Mahee", "mahidhar.mahi030@gmail.com", 27, LocalDate.of(2024, 2, 1), true);
+    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeID") Long employeeId){
+        return new EmployeeDTO(employeeId, "Mahee", "mahidhar.mahi030@gmail.com", 27, LocalDate.of(2024, 2, 1), true);
     }
 
     @GetMapping
-    public String getAllEmployees(@RequestParam Integer age){
-        return "Hi age" + age;
+    public String getAllEmployees(@RequestParam(required = false) Integer age,
+                                  @RequestParam(required = false) String sortBy){
+        return "Hi age" + age+ " "+ sortBy;
     }
 
 }
