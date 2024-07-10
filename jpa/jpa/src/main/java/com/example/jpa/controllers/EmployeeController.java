7f648f95f74cd071022ee69afc8b4a1a3ac9d0cd
Maheedhar_Windows_machine <Maheedhar@Windows>
2024-07-10 00:37:31 -0400

    dto with getters and setters

---
+++ jpa/jpa/src/main/java/com/example/jpa/controllers/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.example.jpa.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+
+//    @GetMapping(path = "/getmymessage")
+//    public String getMySuperSecretMessage(){
+//        return "Secret message: This is my first get message";
+//    }
+
+
+}
